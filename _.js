7302e1f8cf4f6b9545df5eff22f81b06dab400ce
IliasMariosG <57366310+IliasMariosG@users.noreply.github.com>
2021-01-28 21:50:10 +0200

    Implement inRange method

--- _.js
+++ _.js
@@ -4,10 +4,24 @@ const _ = {
     
     const clampedValue = Math.min(lowerClampedValue, upperBound)
     return clampedValue
+  },
+  inRange (number, start, end) {
+    
+    if (!end) {
+      end = start;
+      start = 0
+    }
+    if (start > end) {
+      let temp = end;
+      end = start;
+      start = temp
+    }
+    const isInRange = ((start <= number) && (number < end));
+    return isInRange
   }
 };
 
-
+_.inRange(3, 2, 1)
 
 
 // Do not write or modify code below this line.
